146f59e8f4481f5e5386bfe81fa2b697fb137887
helen2411 <helen.l.gomes@aluno.senai.br>
2025-02-06 17:27:58 -0300

    Create importação.sql

---
+++ importação.sql
@@ -0,0 +1,4 @@
+SELECT * FROM employees where first_name= "Helen";
+insert into employees(birth_date,first_name,last_name,gender,hire_date) values ("2002-11-24","Helen","Laís","F","2024-09-01");
+Set SQL_SAFE_UPDATES =0;
+update employees set last_name="Laís Gomes" where last_name="Laís";
