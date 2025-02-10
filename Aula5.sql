b4b6abe333c645a8cc82bd1595a68f3b64fc8e51
helen2411 <helen.l.gomes@aluno.senai.br>
2025-02-10 15:24:58 -0300

    ...

---
+++ Aula5.sql
@@ -0,0 +1,31 @@
+create database CoresDoBrasil;
+use CoresDoBrasil;
+
+create table clientes(
+	nome Varchar (100), 
+    email Varchar(255) Unique, 
+    telefone Varchar(18)
+);
+
+Insert into clientes(nome,email,telefone) values ("Helen", "helen22@gmail.com","(61) 1234-5678" );
+Insert into clientes(nome,email,telefone) values ("Laís", "","" );
+
+alter table clientes modify column email Varchar(255) unique not null;
+
+select * from clientes;
+
+#produtos
+
+create table produtos(
+	id Int Unique not null,
+    nome Varchar(255) not null,
+    peso Decimal(6,2) not null,
+    cor Varchar(100)
+);
+
+insert into produtos values(1,"Creme hidratante",500,"branco");
+insert into produtos values(2,"Perfume Maracujá",50,"amarelo");
+select * from produtos;
+
+
+
