2420e2ca1f5c73006ff56a7ed692933dc3c62b1c
helen2411 <helen.l.gomes@aluno.senai.br>
2025-02-19 17:46:42 -0300

    ...

---
+++ Esboço_Pizzaria.sql
@@ -0,0 +1,36 @@
+Create database Pizzaria;
+Use Pizzaria;
+
+Create Table Pedidos(
+	id 
+);
+
+Create Table Sabores(
+4 Queijos,
+ Frango com Catupiry,
+Calabresa,
+Lombinho,
+Filé com Cheddar,
+Portuguesa,
+Margherita
+
+);
+
+Create Table Massas(
+Massa comum,
+Massa integral,
+Massa temperada
+);
+
+Create Table Borda(
+Cheddar,
+Catupiry
+);
+
+Create Table Status_Pedido(
+Em produção,
+Entrega,
+Concluído,
+Cancelado
+);
+
